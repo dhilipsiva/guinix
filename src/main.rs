aa0c05aa23fcac8ecde001caf453e1b0b6fdb744
dhilipsiva <dhilipsiva@pm.me>
2023-02-15 22:59:57 +0530

    feat: PAM authentication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+pub fn main() {
+    let service = "guinix";
+    let user = "dhilipsiva";
+    let password = "mypass";
+
+    let mut auth = pam::Authenticator::with_password(service).unwrap();
+    auth.get_handler().set_credentials(user, password);
+    if auth.authenticate().is_ok() && auth.open_session().is_ok() {
+        println!("Successfully opened a session!");
+    } else {
+        println!("Authentication failed =/");
+    }
 }
